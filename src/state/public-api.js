2ec8c8708d438c0b33f2897a540bf23b628e34c2
Monokee <jonathan@patchflyer.com>
2019-01-05 01:34:55 +0100

    Update public-api.js

--- src/state/public-api.js
+++ src/state/public-api.js
@@ -306,16 +306,25 @@ function proxyGetTrap(target, prop) {
     return wrapStateInProxy(assignInstanceProperties(value));
   }
 
+  //TODO: this has to be guarded against methods on prototype.
+  //TODO: mmh. both array mutators and module actions live on the prototype.
+  //TODO: and both have the potential to mutate the state.
+  // TODO: function caching is the wrong approach here. why?
+  // because that would create one intercepted method for every method on the objects prototype and cache it on every instance...
+  // - wouldn't it be more efficient to have a generic function interceptor method that we call?
+  // - no because we need to return a function and that function should not have to be "called" on the outside.
   if (typeof value === 'function') {
 
     return instance.fnCache.get(prop) || (instance.fnCache.set(prop, (...args) => {
 
       if (!ARRAY_MUTATORS.has(prop) || instance.parent === null) {
-        // if not array mutator or
-        // instance doesn't have parent (mutation via methods means we only react on the parent)
+        // mutation via methods means we only react on the parent
+        // if method is not array mutator or instance doesn't have parent to react on
+        // we only apply the function and the return the result.
         return _apply(value, target, args);
       }
 
+      // create shallow target of the array
       const oldTarget = target.slice();
       const result = _apply(value, target, args);
 
